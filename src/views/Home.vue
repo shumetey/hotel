b702c24519a62683ebee7b1bf81d865264c9abc2
shumetey <84983273+shumetey@users.noreply.github.com>
2021-06-04 21:09:01 +0300

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="home">
+
+    <carousel :autoplay="true" :nav="false" caption="Blank Image" img-blank img-alt="Blank image">
+    <template slot="prev"></template>
+    <img src="https://placeimg.com/200/200/any?1">
+    <img src="https://placeimg.com/200/200/any?2">
+    <img src="https://placeimg.com/200/200/any?3">
+    <img src="https://placeimg.com/200/200/any?4">
+    <template slot="next"></template>
+    </carousel>
+
+    </div>
+</template>
+
+<script>
+// import { BCarousel } from 'bootstrap-vue'
+// Vue.component('b-carousel', BCarousel)
+import carousel from 'vue-owl-carousel'
+export default {
+    components: { carousel },
+}
+</script>
